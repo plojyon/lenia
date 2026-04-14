2aef2a7f09586cf2ed49772a1d40d8c0eb1be79a
Yon Ploj <admin@yon.si>
2026-04-15 00:01:07 +0200

    burek

--- openmp/time_openmp.sh
+++ openmp/time_openmp.sh
@@ -7,7 +7,7 @@ RUNS=5
 for n in "${N[@]}"; do
     for strip in "${STRIPS[@]}"; do
         for run in $(seq 1 $RUNS); do
-            out_file="results/${sha}_${strip}_${run}.txt"
+            out_file="results/${n}_${strip}_${run}.txt"
             ./run_lenia.sh "$out_file" \*.c "$n" "$strip"
         done
     done
